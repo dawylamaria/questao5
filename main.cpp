12fce4285e0cd782595b0567af4361afb5b5425b
Dawyla Maria <dawylamaria@gmail.com>
2023-08-15 12:57:31 -0300

    questao5

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+/* Questao5*/
+using namespace std;
+int main(int argc, char** argv) {
+	
+	double metros;
+	
+	cout << "Digite a quantidade de metros:" <<;
+	
+	cin >> metros;
+	
+	cout <<"Conversao para centimetros:" << metros * 100 << endl;
+	system("pause");
+	return 0;
+}
